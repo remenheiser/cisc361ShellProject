dbec001a9ffc28af56b87c69d6aa964d558cd734
Ryan Emenheiser <ryane@ubuntu-1604.eecis.udel.edu>
2019-10-09 14:57:23 -0400

    Freed up almost all memory leaks in the program at this point, just need to figure out memory leaks in CD

--- sh.h
+++ sh.h
@@ -3,13 +3,14 @@
 
 int pid;
 int sh( int argc, char **argv, char **envp);
-char **stringToArray(char *input, char** argv);
+char **stringToArray(char *input, char** argv, int *argsct);
 char readInput(char* buffer);
 char *which(char *command, struct pathelement *pathlist);
 char *where(char *command, struct pathelement *pathlist);
 void list ( char *dir );
 void printenv(char **envp);
 void freeList(struct pathelement *pathlist);
+
 
 #define PROMPTMAX 32
 #define MAXARGS 10
